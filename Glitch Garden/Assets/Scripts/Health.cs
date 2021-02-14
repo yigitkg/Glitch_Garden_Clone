1489aa287007f4b2118317b93dcadc902fe095e4
yigitkg <yigitkg@gmail.com>
2021-02-14 15:29:51 +0300

    First Commit

---
+++ Glitch Garden/Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float healt = 100f;
+    [SerializeField] GameObject deathVFX;
+
+
+    public void DealDamage(float damage)
+    {
+        healt -= damage;
+        if (healt <= 0)
+        {
+            TriggerDeathVFX();
+            Destroy(gameObject);
+        }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX)
+        {
+            return;
+        }
+        GameObject deathVFXObject = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathVFXObject, 1f);
+    }
+
+}
---
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gun;
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+}
+
